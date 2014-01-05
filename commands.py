457c130e9263f3a5d1dbb89a4e09af0dc50dbb96
Manuel Bua <manuel.bua@gmail.com>
2014-01-05 22:32:21 +0100

    Fixed bad comparation

--- commands.py
+++ commands.py
@@ -234,14 +234,11 @@ def cmd_list_next(args):
     next_store = KVStore(NEXT_STORE_FILE)
     repo_info = get_repo_info()
     last_tag = repo_info['last-tag']
-
     has_next_custom = next_store.has(last_tag)
-    next_custom = next_store.get(last_tag) if has_next_custom else None
-
     if not next_store.empty():
         def print_item(k, v):
             print "    %s => %s" % (color_tag(k), color_next(v)) +\
-                  (' (*)' if tag == last_tag else '')
+                  (' (*)' if k == last_tag else '')
 
         print "Currently set NEXT custom strings (*=most recent " \
               "and reachable tag):"
