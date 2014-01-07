d797f021ff9d9c506d643cce71203c5c4848dd0c
Manuel Bua <manuel.bua@gmail.com>
2014-01-07 21:22:28 +0100

    Bump version to 0.2.3

--- gitver/_version.py
+++ gitver/_version.py
@@ -1,6 +1,8 @@
+#!/usr/bin/env python2
+# coding=utf-8
 # this is included for version tracking purposes only
 
 # DO NOT TOUCH, AUTOMATICALLY UPDATED!
-gitver_version = '0.2.2/881744b'
-gitver_buildid = '881744b8bb469b5cb1e87cbcc92a34ae01b5f95d'
-gitver_pypi = '0.2.2'
+gitver_version = '0.2.3'
+gitver_buildid = ''
+gitver_pypi = '0.2.3'
