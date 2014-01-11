fdb3b1e5ddee0571197b23dcc536cd29fe189426
Manuel Bua <manuel.bua@gmail.com>
2014-01-11 23:51:57 +0100

    Add version formatting helper, custom formatting, remove "update-promote"

--- gitver/git.py
+++ gitver/git.py
@@ -18,7 +18,8 @@ except ImportError:
           "run gitver again."
     sys.exit(1)
 
-version_matcher = r"v{0,1}(\d+)\.(\d+)\.(\d+)-(\d+)-g([a-fA-F0-9]+)"
+hash_matcher = r".*-g([a-fA-F0-9]+)"
+tag_matcher = r"v{0,1}(\d+)\.(\d+)\.(\d+)(?:-([0-9A-Za-z-.]*))?"
 
 
 def git_version():
@@ -37,16 +38,16 @@ def project_root():
     return root
 
 
-def describe():
+def describe_hash():
     try:
         tagver = sh.git(
             'describe', '--long', '--match=v*').stdout.replace('\n', '')
-        vm = re.match(version_matcher, tagver).groups()
-        if len(vm) != 5:
+        vm = re.match(hash_matcher, tagver).groups()
+        if len(vm) != 1:
             raise AttributeError
     except (ErrorReturnCode, AttributeError):
         return False
-    return vm
+    return vm[0]
 
 
 def count_tag_to_head(tag):
@@ -88,27 +89,20 @@ def data_from_tag(tag):
 
 def get_repo_info():
     # retrieve basic repository information
-    desc = describe()
-    if not desc:
-        print err("Error, this repository is required to define tags in the "
-                  "format vX.Y.Z")
+    desc_hash = describe_hash()
+    if desc_hash is None or not desc_hash:
+        print err("Error, couldn't describe current hash")
         sys.exit(1)
 
-    # extract version string, assumes HEAD to be at that commit
-    vmaj = int(desc[0])
-    vmin = int(desc[1])
-    vpatch = int(desc[2])
-    vcount = int(desc[3])
-    vhash = desc[4]
-    hashlen = len(vhash)
+    hashlen = len(desc_hash)
     full_build_id = get_build_id()
     if not full_build_id or hashlen == 0:
         print err("Couldn't retrieve build id information")
         sys.exit(1)
 
     # sanity check
-    if not full_build_id.startswith(vhash):
-        print err("Hash problem detected: git-describe reports " + vhash +
+    if not full_build_id.startswith(desc_hash):
+        print err("Hash problem detected: git-describe reports " + desc_hash +
                   ", but HEAD id is " + full_build_id)
         sys.exit(1)
 
@@ -117,6 +111,17 @@ def get_repo_info():
         print err("Couldn't retrieve the latest tag")
         sys.exit(1)
 
+    data = data_from_tag(tag)
+    if data is None:
+        print "Couldn't retrieve version information from tag \"" + tag + "\""
+        sys.exit(1)
+
+    vmaj = int(data[0])
+    vmin = int(data[1])
+    vpatch = int(data[2])
+    pr = data[3]
+    vcount = count_tag_to_head(tag)
+
     return {'maj': vmaj, 'min': vmin, 'patch': vpatch, 'count': vcount,
             'build-id': full_build_id[:hashlen], 'full-build-id': full_build_id,
-            'last-tag': tag}
+            'last-tag': tag, 'pr': pr}
